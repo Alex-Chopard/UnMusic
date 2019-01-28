d87ce3a2e99b1cf2a7e0508e7a713a7df18976d4
Alex Chopard <alex.chopard1998@gmail.com>
2019-01-28 14:36:20 +0100

    Bind seekBar with mediaPlayer

--- app/src/main/java/com/sloubi/unmusic/MainActivity.java
+++ app/src/main/java/com/sloubi/unmusic/MainActivity.java
@@ -2,30 +2,48 @@ package com.sloubi.unmusic;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
-    private Context context;
     private MediaPlayer mediaPlayer;
+    private Handler mHandler = new Handler();
+
+    private SeekBar progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = getBaseContext();
-
 
         ImageView play = findViewById(R.id.iv_play);
-        play.setOnClickListener(this);
+        progress = findViewById(R.id.sb_avancement);
 
-        mediaPlayer = MediaPlayer.create(context, R.raw.igorrr_viande);
+        play.setOnClickListener(this);
+        progress.setOnSeekBarChangeListener(this);
+
+        mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.igorrr_viande);
+        progress.setMax(mediaPlayer.getDuration() / 1000);
+
+        //Make sure you update Seekbar on UI thread.
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(mediaPlayer != null) {
+                    int mCurrentPosition = mediaPlayer.getCurrentPosition() / 1000;
+                    progress.setProgress(mCurrentPosition);
+                }
+                mHandler.postDelayed(this, 1000);
+            }
+        });
     }
 
     @Override
@@ -51,5 +69,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
                 break;
         }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(mediaPlayer != null && fromUser){
+            mediaPlayer.seekTo(progress * 1000);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
