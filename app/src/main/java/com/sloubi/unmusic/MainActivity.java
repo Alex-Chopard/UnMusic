54c76b040db591e0eb0f08136de4c4f6a172ed3c
Alex Chopard <alex.chopard1998@gmail.com>
2019-02-12 09:01:14 +0100

    Remove Spotify libs

--- app/src/main/java/com/sloubi/unmusic/MainActivity.java
+++ app/src/main/java/com/sloubi/unmusic/MainActivity.java
@@ -5,18 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
-import android.widget.Toast;
 
 import com.sloubi.unmusic.service.LocationService;
 
