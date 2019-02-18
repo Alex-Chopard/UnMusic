8435374d2c5aefa0e2908db6d0a735b935fb1bd9
Alex Chopard <alex.chopard1998@gmail.com>
2019-02-18 14:46:35 +0100

    Get playlist from server

--- app/src/main/java/com/sloubi/unmusic/PlayListActivity.java
+++ app/src/main/java/com/sloubi/unmusic/PlayListActivity.java
@@ -1,34 +1,52 @@
 package com.sloubi.unmusic;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.sloubi.unmusic.Adapter.PlaylistAdapter;
+import com.sloubi.unmusic.Interface.OnMusicListDownloadListener;
+import com.sloubi.unmusic.Model.Music;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class PlayListActivity extends AppCompatActivity {
+public class PlayListActivity extends AppCompatActivity implements OnMusicListDownloadListener {
+
+    private ListView playlist;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
 
-        ListView playlist = findViewById(R.id.lv_playlist);
+        playlist = findViewById(R.id.lv_playlist);
+
+        // Get all music.
+        Music.list(this, this);
+    }
+
+    @Override
+    public void onDownloadComplete(List<Music> data) {
         List<HashMap<String, String>> musics = new ArrayList<>();
 
-        HashMap<String, String> music = new HashMap<>();
-        music.put("id", "0");
-        music.put("fullTitle", "Nightcore - 7 rings  God is a woman (Switching Vocals)");
+        for (Music music: data) {
+            HashMap<String, String> musicMap = new HashMap<>();
+            musicMap.put("id", music.getId());
+            musicMap.put("fullTitle", music.getFullTitle());
 
-        musics.add(music);
+            musics.add(musicMap);
+        }
 
         PlaylistAdapter adapter = new PlaylistAdapter(this.getBaseContext(), musics);
-
         playlist.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDownloadError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
     }
 }
